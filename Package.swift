23e3b94ae11902a505b0d1bd63d575457cfad084
Thomas Roughton <t.roughton@me.com>
2021-07-15 10:40:34 +1200

    Update to LodePNG 20210627

--- Package.swift
+++ Package.swift
@@ -20,6 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "LodePNG",
-            dependencies: []),
+            dependencies: [],
+            cSettings: [.define("LODEPNG_NO_COMPILE_CPP")]),
     ]
 )
